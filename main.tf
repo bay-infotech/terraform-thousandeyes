62db2b32aa479004fa417dff8ab5e9b8292666fa
Majid S <m.sotoodeh@gmail.com>
2022-04-09 16:04:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     thousandeyes = {
       source = "william20111/thousandeyes"
-      version = "0.6.0"
+      version = "0.3.7"
     }
   }
 }
@@ -10,27 +10,27 @@ provider "thousandeyes" {
   token = var.te_token
 }
 data "thousandeyes_agent" "ce_agent" {
-  agent_name  = var.ce_agent_name
+  name  = var.ce_agent_name
 }
-# resource "thousandeyes_agent_to_server" "server_test" {
-  # test_name = "my agent test"
-  # interval = 120
-  # server = "8.8.8.8"
-  # agents {
-      # agent_id = data.thousandeyes_agent.ce_agent.agent_id
-  # }
-
-# }
-
-
-resource "thousandeyes_http_server" "google_http_test" {
-  test_name = "google test"
+resource "thousandeyes_agent_to_server" "server_test" {
+  test_name = "my agent test"
   interval = 120
-  url = "https://google.com"
+  server = "8.8.8.8"
   agents {
       agent_id = data.thousandeyes_agent.ce_agent.agent_id
   }
-  agents {
-      agent_id = 12345
-  }
+
 }
+
+
+# resource "thousandeyes_http_server" "google_http_test" {
+  # test_name = "google test"
+  # interval = 120
+  # url = "https://google.com"
+  # agents {
+      # agent_id = data.thousandeyes_agent.ce_agent.agent_id
+  # }
+  # agents {
+      # agent_id = 12345
+  # }
+# }
