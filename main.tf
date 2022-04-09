f47f50afa35170f529b552b1937b6ce44bd69298
Majid S <m.sotoodeh@gmail.com>
2022-04-09 15:32:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,25 +6,18 @@ terraform {
     }
   }
 }
-
-
 provider "thousandeyes" {
   token = var.te_token
 }
-
-
-data "thousandeyes_agent" "test_agent" {
+data "thousandeyes_agent" "ce_agent" {
   agent_name  = var.ce_agent_name
-
 }
-
-
-resource "thousandeyes_agent_to_server" "agent_test_example" {
+resource "thousandeyes_agent_to_server" "server_test" {
   test_name = "my agent test"
   interval = 120
-  server = "google.com"
+  server = "8.8.8.8"
   agents {
-      agent_id = data.thousandeyes_agent.test_agent.agent_id
+      agent_id = data.thousandeyes_agent.ce_agent.agent_id
   }
 
 }
@@ -35,10 +28,9 @@ resource "thousandeyes_http_server" "google_http_test" {
   interval = 120
   url = "https://google.com"
   agents {
-      agent_id = data.thousandeyes_agent.test_agent.agent_id
+      agent_id = data.thousandeyes_agent.ce_agent.agent_id
   }
   agents {
-
       agent_id = 12345
   }
 }
