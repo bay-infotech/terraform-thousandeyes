bcb628c162ee91b45acdc71314b8ae1bcdab8d25
Majid S <m.sotoodeh@gmail.com>
2022-04-09 14:58:29 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,31 +12,33 @@ provider "thousandeyes" {
   token = var.te_token
 }
 
-data "thousandeyes_agent" "ce_agent" {
-  agent_name  = var.ce_agent_name
+
+data "thousandeyes_agent" "thousandeyes-va" {
+  name  = "thousandeyes-va"
+
 }
 
 
-# resource "thousandeyes_agent_to_server" "agent_to_google_com" {
-  # test_name = "agent_to_google.com"
-  # interval = 120
-  # server = "google.com"
-  # agents {
-      # agent_id = data.thousandeyes_agent.ce_agent.agent_id
-  # }
+resource "thousandeyes_agent_to_server" "agent_to_google_com" {
+  test_name = "agent_to_google.com"
+  interval = 120
+  server = "google.com"
+  agents {
+      agent_id = data.thousandeyes_agent.thousandeyes-va.agent_id
+  }
 
-# }
+}
 
 
-# resource "thousandeyes_http_server" "google_http_test" {
-  # test_name = "google test"
-  # interval = 120
-  # url = "https://google.com"
-  # agents {
-      # agent_id = data.thousandeyes_agent.ce_agent.agent_id
-  # }
-  # agents {
+resource "thousandeyes_http_server" "google_http_test" {
+  test_name = "google test"
+  interval = 120
+  url = "https://google.com"
+  agents {
+      agent_id = data.thousandeyes_agent.thousandeyes-va.agent_id
+  }
+  agents {
 
-      # agent_id = 12345
-  # }
-# }
+      agent_id = 12345
+  }
+}
